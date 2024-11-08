79f5e4f0c8ac09f1c493fcc737d82bdde23eebe4
jjong <90lastone@gmail.com>
2024-11-08 14:58:40 +0900

    feat: 토픽 추가 테스트 api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AddTopicReqDto } from './app.dts';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('add-topic')
+  async addTopic(@Body() dto: AddTopicReqDto): Promise<string> {
+    const { topic } = dto;
+    if (topic == undefined) {
+      return 'topic is undefined';
+    } else {
+      await this.appService.addSubscriptionTopic(topic);
+      return `topic ${topic} added`;
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { EachMessagePayload, Kafka } from 'kafkajs';
 
 @Injectable()
 export class AppService {
+  private kafka = new Kafka({
+    clientId: 'nestjs-kafka',
+    brokers: ['localhost:9094'],
+  });
+  private readonly producer = this.kafka.producer();
+  private readonly consumer = this.kafka.consumer({
+    groupId: 'nestjs-kafka-group',
+  });
+
+  constructor() {
+    this.consumer.connect();
+    this.consumer.subscribe({ topics: ['test_a', 'test_b'] });
+    this.consumer.run({
+      eachMessage: this.consumerCallback,
+    });
+  }
+
+  async consumerCallback(payload: EachMessagePayload) {
+    console.log('kafka message arrived');
+    console.log(
+      `topic: ${payload.topic}, Message:${payload.message.value.toString()}`,
+    );
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async addSubscriptionTopic(topic: string) {
+    await this.consumer.stop();
+    await this.consumer.subscribe({ topic }); // 구독하고
+    await this.consumer.run({
+      eachMessage: this.consumerCallback,
+    });
   }
 }
