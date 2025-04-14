9bc91b306d58232a32e74c5cdfc8f2eb38d1c71b
jjong <akrTmwk@gmail.com>
2025-04-14 14:54:24 +0900

    refactor: app kafka api 제거

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MetaModule } from './meta/meta.module';
-import { KafkaModule } from '@kafka';
 
 @Module({
-  imports: [KafkaModule, MetaModule],
+  imports: [MetaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/app/src/app.service.ts
+++ apps/app/src/app.service.ts
@@ -1,29 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { SendMessageReqDto } from './app.dts';
-import { KafkaProducerService } from '@kafka/kafka-producer.service';
-import { KafkaConsumerService } from '@kafka/kafka-consumer.service';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly kafkaProducerService: KafkaProducerService,
-    private readonly kafkaConsumerService: KafkaConsumerService,
-  ) {}
+  constructor() {}
 
   getHello(): string {
     return 'Hello app!';
-  }
-
-  async addTopic(topic: string) {
-    await this.kafkaConsumerService.subscribe(topic, async (message) => {
-      // 메시지 처리 로직
-      console.log(message);
-      return true;
-      // TODO 메시지 처리 로직 실패 false
-    });
-  }
-
-  async sendMessage(dto: SendMessageReqDto) {
-    await this.kafkaProducerService.sendMessage(dto);
   }
 }
