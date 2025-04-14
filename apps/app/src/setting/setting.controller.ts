9db9def6677e268a2e92cf54ae306c8f96ce0586
jjong <akrTmwk@gmail.com>
2025-04-14 12:20:01 +0900

    refactor: setting 미사용 제거

--- apps/app/src/setting/setting.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SettingService } from './setting.service';
-
-@Controller()
-export class SettingController {
-  constructor(private readonly settingService: SettingService) {}
-}
