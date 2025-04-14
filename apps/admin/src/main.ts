538d47387630ec6084693fbb84b9871775c4e183
jjong <akrTmwk@gmail.com>
2025-04-14 14:38:55 +0900

    chore: admin main port 3001, swagger 설정

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('nestjs kafka admin')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(3001);
 }
 bootstrap();
